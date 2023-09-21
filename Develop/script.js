5f572959aa2d9b1c2ac624dc0a7e208d6b081830
tanfei0704 <tanfei0704@gmail.com>
2023-09-21 00:09:09 -0400

    update homework in cloud

---
+++ Develop/script.js
@@ -0,0 +1,76 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Get the placeholder element
+var passwordPlaceholder = document.querySelector("#password");
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+function writePassword() {
+  var passwords = generatePasswords(); // Call the generatePasswords function
+  passwordPlaceholder.value = passwords; // Display the generated passwords in the textarea
+}
+
+  // Prompt for the desired length of passwords
+  function generatePasswords(){
+  var passwordLength = prompt("Please enter the desired length for the passwords.");
+  
+  // Validate the password length input
+  if (passwordLength === null || isNaN(passwordLength)) {
+    alert("Please enter a valid number for the password length.");
+    return generatePasswords();
+  } else if (passwordLength < 1) {
+    alert("Password length must be greater than 0.");
+    return;
+  }
+  passwordLength = parseInt(passwordLength);
+  // console.log(passwordLength);
+  
+  // Confirm the types of characters from user input
+  var useLowercase = confirm("Do you want lowercase characters in your passwords?");
+  var useUppercase = confirm("Do you want uppercase characters in your passwords?");
+  var useNumber = confirm("Do you want numbers in your passwords?");
+  var useSymbol = confirm("Do you want special characters in your passwords?");
+
+  // Check if at least one character type is selected
+  if (!(useLowercase || useUppercase || useNumber || useSymbol)) {
+    alert("You must select at least one character type.");
+    return confirm();
+  }
+  // Define character sets based on user choices
+  var lowercasePW = "abcdefghijklmnopqrstuvwxyz";
+  var uppercasePW = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var numberPW = "0123456789";
+  var symbolPW = "!@#$%^&*()_+{}[]|:;<>,.?/";
+
+  var allPW = "";
+  
+  if (useLowercase) {
+    allPW += lowercasePW;
+  }
+  if (useUppercase) {
+    allPW += uppercasePW;
+  }
+  if (useNumber) {
+    allPW += numberPW;
+  }
+  if (useSymbol) {
+    allPW += symbolPW;
+  }
+
+  var passwords = [];
+
+  // Generate the passwords
+  for (var p = 0; p < passwordLength; p++) {
+    var password = "";
+    for (var i = 0; i < passwordLength; i++) {
+      var randomIndex = Math.floor(Math.random() * allPW.length);
+      password += allPW.charAt(randomIndex);
+    }
+    passwords.push(password);
+    
+  }
+   return password;
+  }
+
--- README.md
+++ README.md
@@ -1,1 +1,41 @@
 # challenge-three-passcode-generator
+create a password generator by click button
+
+# Project Title
+Password Generator
+
+# Description
+This project is to create a password generator which could generate the passwords regarding user's input by using JavaScript language. It is a modify starter code that should be run in the broswer and can generate a random password that meets certain creteria.
+
+# Technologies Used
+JavaScript, HTML
+
+# Acceptance Criteria
+
+```
+GIVEN I need a new, secure password
+WHEN I click the button to generate a password
+THEN I am presented with a series of prompts for password criteria
+WHEN prompted for password criteria
+THEN I select which criteria to include in the password
+WHEN prompted for the length of the password
+THEN I choose a length of at least 8 characters and no more than 128 characters
+WHEN asked for character types to include in the password
+THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
+WHEN I answer each prompt
+THEN my input should be validated and at least one character type should be selected
+WHEN all prompts are answered
+THEN a password is generated that matches the selected criteria
+WHEN the password is generated
+THEN the password is either displayed in an alert or written to the page
+
+# Usage:
+1.create a folder;
+2.clone link " " in your foler on terminal;
+3.open index.html file;
+4.right click on mouse to look through on default broswer;
+
+Or you can directly copy& paste link "" on your broswer.
+
+The web should be looked like:
+![picture](./Assets/03-javascript-homework-demo.png)
